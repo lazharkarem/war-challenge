fcb9b335da7fe4600ecc386d479a9debd7fe9f2b
lazharkarem <karem.lazhar@gmail.com>
2021-12-15 23:00:31 +0100

    init war-challenge project

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Petrove Merak on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
